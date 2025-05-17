4e77780da4a909ef48a05aa5019cd6b48be0e687
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-05-17 21:59:03 +0530

    done

---
+++ task.js
@@ -0,0 +1,23 @@
+// index26.js
+
+export default function greet(name) {
+    return Hello, ${name}
+}
+
+
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+export function multiply(a, b) {
+    return a * b;
+}
+
+export function divide(a, b) {
+    return a / b;
+}
