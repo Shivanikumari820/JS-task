b3d63aed233bc6a8a7434b66cf43d90d67e357f7
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-05-21 23:41:24 +0530

    done

--- 25.js
+++ 25.js
@@ -1,7 +1,13 @@
-  function divide(a, b) {
-      if (b === 0) {
-          throw new Error("Cannot divide by zero!");
-      }
-      return a / b;
-  }
-  
+  // Q.25
+        // function checkSecondNumber(num1, num2) {
+        // if (num2 === 0) {
+        //     throw new Error('Error: Second number cannot be zero');
+        // }
+        // return num1 / num2;
+        // }
+        // try {
+        // console.log(checkSecondNumber(10, 0));
+        // } catch(e) {
+        // console.error(e.message);
+        // }
+
