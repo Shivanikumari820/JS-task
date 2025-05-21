9254919ae5126dd842dbbd7e245a605a06aa653e
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-05-21 23:42:01 +0530

    done

--- 24.js
+++ 24.js
@@ -1,6 +1,12 @@
-  function checkInteger(num) {
-      if (!Number.isInteger(num)) {
-          throw new Error("Not an integer!");
-      }
-  }
-  
+  // Q.24
+        // function insrt(num) {
+        // if (!Number.isInteger(num)) {
+        //     throw new Error('Error: Number is not an integer');
+        // }
+        // return 'Number is integer';
+        // }
+        // try {
+        // console.log(insrt(4.5));
+        // } catch(e) {
+        // console.error(e.message);
+        // }
