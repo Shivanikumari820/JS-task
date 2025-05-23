4d8cbc7bb31f83d110295e63c5f17b67872e9368
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-05-23 21:28:15 +0530

    done

---
+++ 21.js
@@ -0,0 +1,7 @@
+  // Q.21
+        // function styl() {
+        // const p = document.getElementById('text');
+        // p.style.color = 'blue';
+        // p.style.fontSize = '40px';
+        // p.style.fontWeight = 'bold';
+        // }
