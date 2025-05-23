4f2082f06a51d116fa173f70c775b6bfa6b10b79
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-05-23 21:29:09 +0530

    done

---
+++ 20.js
@@ -0,0 +1,4 @@
+// Q.20
+        // let value = "<div>Hello</div>";
+        // let isHtml = value.includes("<") && value.includes(">") && value.indexOf("<") < value.indexOf(">");
+        // console.log(isHtml);
